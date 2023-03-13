8ae131a61f438a3cfd117c41fcb51b4f4f60bc97
Antonnyo Carlos <127267978+Antonnyocarlos93@users.noreply.github.com>
2023-03-13 11:44:00 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,31 @@
+/*
+objetivo - quando clicarmos no botão temos que
+mostrar a imagem de fundo correspodente
+- =-
+
+
+*/
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+     botao.addEventListener('click', () => {
+       
+        const botaoSelecinado = document.querySelector('.selecionado');
+        botaoSelecinado.classList.remove('selecionado');
+
+        botao.classList.add('selecionado');
+
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        imagens[indice].classList.add('ativa');
+
+    })
+})
+
+
+
+
+
